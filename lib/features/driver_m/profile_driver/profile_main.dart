fcdc8bc81b3f60a16af030b22025eec7a50c1b7c
Marina Saad <eng8marinasaad@gmail.com>
2026-04-22 13:22:49 +0200

    day

--- lib/features/driver_m/profile_driver/profile_main.dart
+++ lib/features/driver_m/profile_driver/profile_main.dart
@@ -1,3 +1,26 @@
+// import 'package:flutter/material.dart';
+// import 'package:voya/features/driver_m/home/app_bar_screen.dart';
+// import 'package:voya/features/driver_m/profile_driver/profile.dart';
+
+// class DriverProfileScreen extends StatelessWidget {
+//   const DriverProfileScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             AppBarScreen(text1: 'My Profile', text2: ''),
+//             const SizedBox(height: 20),
+//             const ProfileScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:voya/features/driver_m/home/app_bar_screen.dart';
 import 'package:voya/features/driver_m/profile_driver/profile.dart';
@@ -8,14 +31,11 @@ class DriverProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            AppBarScreen(text1: 'My Profile', text2: ''),
-            const SizedBox(height: 20),
-            const ProfileScreenUI(),
-          ],
-        ),
+      body: Column(
+        children: [
+          AppBarScreen(text1: 'My Profile', text2: ''),
+          Expanded(child: SingleChildScrollView(child: const ProfileScreen())),
+        ],
       ),
     );
   }
